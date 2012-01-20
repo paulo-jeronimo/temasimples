c8d58766eb13b70a1af68f599433484cdecbfdcd
Paulo Rodrigues <parodrigues.costa@gmail.com>
2012-01-20 20:41:23 -0200

    comentario

---
+++ footer.php
@@ -0,0 +1,11 @@
+        </div><!-- #container -->
+       	<footer id="rodape">
+            <!-- Conteudo do rodape -->
+        </footer>
+    </div> <!-- #main -->	
+	<?php 
+	//Sempre deixa essa função wp_footer(); pois alguns plugins utilizam dela para retornar informação
+	wp_footer(); 
+	?>
+</body>
+</html>
